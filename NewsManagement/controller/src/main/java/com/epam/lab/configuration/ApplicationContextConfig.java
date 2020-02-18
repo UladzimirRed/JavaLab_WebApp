27dda3902440600916613997e4494d11e952b317
Uladzimir Redzko <uladzimir_redzko@epam.com>
2020-02-18 15:47:12 +0300

    Correction of defects after the review (part 2)

--- NewsManagement/controller/src/main/java/com/epam/lab/configuration/ApplicationContextConfig.java
+++ NewsManagement/controller/src/main/java/com/epam/lab/configuration/ApplicationContextConfig.java
@@ -2,8 +2,10 @@ package com.epam.lab.configuration;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@EnableWebMvc
 @ComponentScan(basePackages = "com.epam.lab")
 public class ApplicationContextConfig {
 }
--- NewsManagement/controller/src/main/java/com/epam/lab/configuration/WebInitializer.java
+++ NewsManagement/controller/src/main/java/com/epam/lab/configuration/WebInitializer.java
@@ -5,12 +5,11 @@ import org.springframework.web.context.support.AnnotationConfigWebApplicationCon
 import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
 public class WebInitializer implements WebApplicationInitializer {
     @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
+    public void onStartup(ServletContext servletContext) {
         AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext();
         applicationContext.register(ApplicationContextConfig.class);
         ServletRegistration.Dynamic dispatcher = servletContext.addServlet("SpringDispatcher",
